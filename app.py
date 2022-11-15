853bf4c31169f721f557a86ebdcb19ad96d0998c
Bohdan Pavliukh <bohdanpavliukh@gmail.com>
2022-11-15 14:22:19 +0200

    init 1

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+if __name__ == '__main__':
+    server_port = os.environ.get('PORT', '7777')
+    app.run(debug=True, port=server_port, host='0.0.0.0')
